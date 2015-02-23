5063f9c7e610354b577bf89a91c9c8c71bd4e471
Christoph Donges <cdonges@gmail.com>
2015-02-23 13:53:42 +1000

    basic knock detection

--- knock/knock.ino
+++ knock/knock.ino
@@ -1,18 +1,41 @@
 #include <LiquidCrystal.h>
 
-int piezo = 10;
+#define KNOCKS 10
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+int threshhold = 50;
+int gapWait = 500;
+
+unsigned long startTime = -1;
+unsigned long lastHit = -1;
+
+int knocks[KNOCKS];
+int pos = 0;
+
 void setup()
 {
-  pinMode(piezo, OUTPUT);
-
   lcd.begin(16, 2);
   lcd.clear();
 }
 
 void loop()
 {
-  lcd.print("test");
+  int knock = analogRead(0);
+  if (knock > threshhold && pos < KNOCKS)
+  {
+    if (startTime == -1)
+    {
+      startTime = millis();
+    }
+   
+     if ((lastHit + gapWait) < millis()) 
+     {
+      lcd.clear();
+      lcd.print(knock);
+      lastHit = millis();
+      knocks[pos] = knock;
+      pos++;
+     }
+   }
 }
