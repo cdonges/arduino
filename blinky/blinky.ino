41ce98f86a877807f4f772156747d15432469eaa
Christoph Donges <cdonges@gmail.com>
2015-05-01 17:24:01 +1000

    fixes

--- blinky/blinky.ino
+++ blinky/blinky.ino
@@ -1,42 +1,69 @@
-int cols[] = {8,9,10,11};
-int rows[] = {12,13};
+int cols[] = {8, 9, 10, 11};
+int rows[] = {12, 13};
 
-int display[2][4] = {{0,1,1,1},{1,1,0,1}};
+int display[2][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}};
 
 void setup() {
   for (int i = 0; i < 4; i++)
   {
-      pinMode(cols[i], OUTPUT);
+    pinMode(cols[i], OUTPUT);
   }
 
   for (int i = 0; i < 2; i++)
   {
-      pinMode(rows[i], OUTPUT);
+    pinMode(rows[i], OUTPUT);
   }
 }
 
 void loop() {
-  Write();
+  for (int i = 0; i < 4; i++)
+  {
+    Clear();
+    display[0][i] = 1;
+    display[1][3 - i] = 1;
+    Write(200);
+  }
+
+  for (int i = 0; i < 4; i++)
+  {
+    Clear();
+    display[0][3 - i] = 1;
+    display[1][i] = 1;
+    Write(200);
+  }
 }
 
-void Write()
+void Clear()
 {
-   for (int row = 0; row < 2; row++)
+  for (int i = 0; i < 4; i++)
   {
-     digitalWrite(rows[row == 0 ? 1 : 0], HIGH);
-     for (int col = 0; col < 4; col++)
-    {
-      if (display[row][col] == 1)
-      {
-        digitalWrite(cols[col], HIGH);
-      }
-      else
-      {
-        digitalWrite(cols[col], LOW);
-      }
-     } 
-        digitalWrite(rows[row], LOW);
-      delay(10);
+    display[0][i] = 0;
+    display[1][i] = 0;
   }
- 
+}
+
+void Write(int ms)
+{
+  int startTime = millis();
+
+  while (millis() - startTime <= ms)
+  {
+    for (int row = 0; row < 2; row++)
+    {
+      digitalWrite(rows[row == 0 ? 1 : 0], HIGH);
+      for (int col = 0; col < 4; col++)
+      {
+        if (display[row][col] == 1)
+        {
+          digitalWrite(cols[col], HIGH);
+        }
+        else
+        {
+          digitalWrite(cols[col], LOW);
+        }
+      }
+      digitalWrite(rows[row], LOW);
+      delay(10);
+    }
+  }
 }
